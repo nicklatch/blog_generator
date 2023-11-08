bc394bc31ee591e363f251b99094985f30fd5097
Nick <nick.latcham@proton.me>
2023-11-08 10:58:06 -0600

    ..

--- hello.hs
+++ hello.hs
@@ -9,9 +9,9 @@ main =
 myhtml :: Html
 myhtml =
   html_
-    "My title"
+    "<title></title>"
     ( append_
-        (h1_ "Heading")
+        (h1_ "<h1></h1>")
         ( append_
             (p_ "Paragraph #1")
             (p_ "Paragraph #2")

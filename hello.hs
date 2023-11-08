4cf7e4dae04afced44781c62fb26e5685aeaedc4
Nick <nick.latcham@proton.me>
2023-11-08 11:29:59 -0600

    ..

--- hello.hs
+++ hello.hs
@@ -1,17 +1,15 @@
 -- hello.hs
-
 import Html
 
 main :: IO ()
-main =
-  putStrLn (render myhtml)
+main = putStrLn (render myhtml)
 
 myhtml :: Html
 myhtml =
   html_
-    "<title></title>"
+    "My title"
     ( append_
-        (h1_ "<h1></h1>")
+        (h1_ "Heading")
         ( append_
             (p_ "Paragraph #1")
             (p_ "Paragraph #2")

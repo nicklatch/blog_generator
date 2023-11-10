2928f20df63ec636afd249eb3926fa6c42f3c662
Nick <nick.latcham@proton.me>
2023-11-10 10:21:33 -0600

    feat(markup): adds parsing of structure types

--- Markup.hs
+++ Markup.hs
@@ -8,6 +8,8 @@ where
 
 import Numeric.Natural
 
+-- * Types
+
 type Document = [Structure]
 
 data Structure
@@ -16,3 +18,24 @@ data Structure
   | UnorderedList [String]
   | OrderedList [String]
   | CodeBlock [String]
+
+-- * Parsing
+
+parse :: String -> Document
+parse = parseLines [] . lines
+
+parseLines :: [String] -> [String] -> Document
+parseLines currentParagraph txts =
+  let paragraph = Paragraph (unlines (reverse currentParagraph))
+   in case txts of
+        [] ->
+          [paragraph]
+        currentLine : rest ->
+          if trim currentLine == ""
+            then paragraph : parseLines [] rest
+            else parseLines (currentLine : currentParagraph) rest
+
+trim :: String -> String
+trim = unwords . words
+
+-- TODO: Left off at https://learn-haskell.blog/04-markup/03-displaying_results.html
